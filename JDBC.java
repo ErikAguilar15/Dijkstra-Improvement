4153683cfa3ebcc2553b6d4e57d8b7a1c94579c3
jgreen24 <35980201+jgreen24@users.noreply.github.com>
2019-04-04 04:00:23 -0700

    Added Open/Close methods
    Connecting locally.

--- JDBC.java
+++ JDBC.java
@@ -8,32 +8,57 @@ import java.sql.Statement;
 public class JDBC {
 	
 	//static String DBLocation = "jdbc:microsoft:sqlserver://HOST:1433;databaseName=MNFLD.dbo";
+	
+	//DBLocation is local to my computer, theres a way to make remote accessible, ill do later
 	static String DBLocation = "jdbc:sqlserver://JUSTIN:1433;databaseName=MNFLD;integratedSecurity=true";
 	static Connection connection = null;
 	
 	//Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
-	public static void SelectQuery() { //change name of func later, example one
+	
+	//connects to SQL Server DB
+	public static void openSQLConnection() {
+		try {
+			connection = DriverManager.getConnection(DBLocation);
+	
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+			}	
+	}
+	
+	//disconnects from DB
+	private static void closeSQLConnection() {
+
+		if (connection != null) {
+			try {
+				connection.close();
+			}
+			catch (SQLException e) {
+				System.err.println(e.getMessage());
+			}
+		}
+		System.out.println("Disconnected");
+	}
+	
+	
+	public static void getOpenConnectionPorts() { //change name of func later, example one
 		try {	
-			
-		        connection = DriverManager.getConnection(DBLocation);
 		        if (connection != null) {
 		        	System.out.println("Connected");
 		        }
+		        
 		            Statement stat = connection.createStatement();
-		            ResultSet result = stat.executeQuery("SELECT TOP (10) SYS_I "
-		            		+ ",SITE_SYS_I"
-		            		+ ",CNCT_I"
-		            		+ ",ALT_CNCT_I"
-		            		+ ",ACTV_X"
-		            		+ ",RESV_IN_USE_N"
-		            		+ ",CNCT_PT_TYP_SYS_I"
-		            		+ "FROM wmgma01_cnct_pt ");
+		            ResultSet result = stat.executeQuery("SELECT *"
+		            		+ "FROM MNFLD.dbo.wmgma01_cnct_pt AS cp "
+		            		+ "WHERE cp.ACTV_X =0 AND cp.RESV_IN_USE_N < 2"
+		            		+ "ORDER BY cp.RESV_IN_USE_N ASC, cp.CNCT_I");
 		            
-		            if (result.next()) {
-		            	int resultInt = result.getInt("SYS_I");
-		            	System.out.println(resultInt);
+		            while (result.next()) {
+		            	int i = 1;
+		            	System.out.println(result.getString(i));
+		            	i++;
 		            }
 		        }
+	
 		        catch(SQLException e) {
 		        System.err.println(e.getMessage());
 		        }
@@ -41,14 +66,11 @@ public class JDBC {
 
 		
 		public static void main(String[] args) {
-			try {
-				connection = DriverManager.getConnection(DBLocation);
-		
-			} catch(SQLException e) {
-				System.err.println(e.getMessage());
-				}
+			openSQLConnection();
 			
-			SelectQuery();
+			getOpenConnectionPorts();
+			
+			closeSQLConnection();
 		}
 
 
