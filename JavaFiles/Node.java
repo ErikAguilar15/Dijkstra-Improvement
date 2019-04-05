703aac3be6290e3a1304fbab1b0da99359b84630
nelsons707 <43331074+nelsons707@users.noreply.github.com>
2019-04-05 12:26:05 -0700

    Add files via upload

--- JavaFiles/Node.java
+++ JavaFiles/Node.java
@@ -2,36 +2,52 @@ import java.util.*;
 
 public class Node {
 
-	public int pannel;
-	public int port;
+    public int panel;
+    public int port;
 
-	public int srcPortID;
-	public int destPortID;
-	public int value;
+    public int srcPortID;
+    public int destPortID;
+    public int value;
 
-	public List<Node> neighbors = new ArrayList<Node>(); 
+    public List<Node> neighbors = new ArrayList<Node>();
 
-	// Constructor
-	public Node(int s, int d, int v) {
-        	srcPortID = s;
-        	destPortID = d;
-        	value = v;
-    	}
+    //These actually need to be priority queues rather than HashSets
+    Set<Node> visitedNodes = new HashSet<Node>();
+    Set<Node> unvisitedNodes = new HashSet<Node>();
 
-	// Update Node cost
-    	public void updateCost(int v) {
-		value = v;
-	}
+    // Constructor
+    public Node(int s, int d, int v) {
+        this.srcPortID = s;
+        this.destPortID = d;
+        this.value = v;
+    }
 
-	// Get cost of Node
-	public int getCost() {
-		return value;
-	}
+    //the actual shortest path from source to destination
+    private List<Node> shortestPath = new LinkedList<>();
 
-	// Inserting to neighbors list
-	public int addNeighbor(Node n) {
-		neighbors.add(n);
-	}
+    // Update Node cost
+    public void updateCost(int v) {
+        this.value = v;
+    }
+
+
+    // Get cost of Node
+    public int getCost() {
+        return value;
+    }
+
+    // Inserting to neighbors list
+    public void  addNeighbor(Node n) {
+        neighbors.add(n);
+    }
+
+    public int compare(Node node1, Node node2) {
+        if (node1.value < node2.value)
+            return -1;
+        if (node1.value > node2.value)
+            return 1;
+        return 0;
+    }
 
 
 }
