6416571c2151f3964b227245db3de09bf74800ac
ErikAguilar2418 <Trojan24#>
2019-04-17 12:39:27 -0700

    Comments to Dijkstra file

--- JavaFiles/Dijkstra.java
+++ JavaFiles/Dijkstra.java
@@ -9,22 +9,27 @@ public class Dijkstra {
 
         src.setCost(0);
 
+        //TreeSet data structures for nodes we need to visit and add them once visited
         TreeSet<Node> visitedNodes = new TreeSet<>();
         TreeSet<Node> unVisitedNodes = new TreeSet<>();
 
+        //Add root node to visited and everything else to unvisited
         visitedNodes.add(src);
         unVisitedNodes.addAll(g.getNodes());
 
+        //While we still have unvisited nodes
         while (!unVisitedNodes.isEmpty()) {
             Node openNode = unVisitedNodes.first();
 
             unVisitedNodes.remove(openNode);
 
+            //Begin building neighborlist and get path cost
             for (Map.Entry<Node, Integer> n :
                     openNode.getNeighborsList().entrySet()) {
                 Node adjNode = n.getKey();
                 Integer edgeCost = n.getValue();
 
+                //Compare path costs to get shortest path
                 if (!visitedNodes.contains(adjNode)) {
                     if (openNode.getCost() + edgeCost < adjNode.getCost()) {
                         adjNode.setCost(openNode.getCost() + edgeCost);
@@ -44,22 +49,27 @@ public class Dijkstra {
 
         src.setCost(0);
 
+        //TreeSet data structures for nodes we need to visit and add them once visited
         TreeSet<Node> visitedNodes = new TreeSet<>();
         TreeSet<Node> unVisitedNodes = new TreeSet<>();
 
+        //Root nodes
         visitedNodes.add(src);
         unVisitedNodes.add(src);
 
+        //While we still have unvisited nodes
         while (!unVisitedNodes.isEmpty()) {
 
             Node openNode = unVisitedNodes.first();
             unVisitedNodes.remove(openNode);
 
+            //Get node keys and lengths of wine lines
             for (Map.Entry<Node, Integer> n :
                     openNode.getNeighborsList().entrySet()) {
                 Node adjNode = n.getKey();
                 Integer lineLength = n.getValue();
 
+                //Calculate path costs based off line lengths 
                 if (!visitedNodes.contains(adjNode)) {
                     if (openNode.getCost() + lineLength < adjNode.getCost()) {
                         adjNode.setCost(openNode.getCost() + lineLength);
