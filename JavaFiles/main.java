839ec810851ef1e6daf6069dfe0daf9d96ac52e4
marioOrtegaGarcia <mario.j.ortega.garcia@gmail.com>
2019-05-16 15:38:56 -0700

    Added live demo function which allows us to test different line ups

--- JavaFiles/main.java
+++ JavaFiles/main.java
@@ -1,44 +1,11 @@
-import java.util.*;
+import java.util.ArrayList;
 public class main {
 
 	public static void main(String[] args) {
 
-
-		 ArrayList<Node> newnodes = new ArrayList<Node>();
-		
-		 Graph mnfld = new Graph();
-		 
-		 
-		 //might need to limit query results, too large maybe?
-		 //newnodes = JDBC.graphInformation(); //returns arraylist of nodes
-		 //mnfld.setPipes(newnodes); //set nodes to the manifold
-		 //JDBC.insertConnections(mnfld);
-		 
-		
-		 
-		 
-		 
-		 
-		 
-		 /*
-			for (int i = 0; i < newnodes.length(); i++) { //length might be wrong
-				//need to look for way to add edges by looking for neighbor nodes i think
-				//loop through nodes and create Edges
-				//might need new query
-				newedges.addEdge(node1,node2,cost);
-			}
-
-			mnfld.setConnections(newedges);
-
-			up to this point, graph should be global to Dijkstra and unused.
-			loop until user leaves page
-			get input from user for tanks to transfer
-			find shortest path between both tanks
-			end loop
-			this will change depending how html works so not spending any time now on it
-*/
-         //shortestPath.runTest();
-		 shortestPath.runTest2();
+        ArrayList<Node> newnodes = new ArrayList<Node>();
+        shortestPath.liveDemo();
+//		shortestPath.runTest2();
 	}
 
 }
--- JavaFiles/shortestPath.java
+++ JavaFiles/shortestPath.java
@@ -1,4 +1,5 @@
 import java.util.Hashtable;
+import java.util.Scanner;
 
 public class shortestPath {
     /*
@@ -174,5 +175,161 @@ public class shortestPath {
 
     }
 
+    public static void loadGraph(Graph mnfld) {
+        Hashtable<Character, Node> nTable = new Hashtable<Character, Node>();
+//        float destTank = 8f;
+        Character c = 'A';
+
+        // Adding Tanks
+        for (int id = 0; id < 11; id += 2) {
+            nTable.put( c, new Node( (float) id, 5.0f ) );
+            c++;
+        }
+
+        // Adding Pipes
+        nTable.put( 'G', new Node( 1000f, 1001f, 100f ) );
+        nTable.put( 'H', new Node( 1002f, 1003f, 10f ) );
+        nTable.put( 'I', new Node( 1004f, 1005f, 120f ) );
+        nTable.put( 'J', new Node( 1006f, 1007f, 100f ) );
+        nTable.put( 'K', new Node( 1008f, 1009f, 170f ) );
+        nTable.put( 'L', new Node( 1010f, 1011f, 100f ) );
+        nTable.put( 'M', new Node( 1012f, 1013f, 100f ) );
+        nTable.put( 'N', new Node( 1014f, 1015f, 100f ) );
+        nTable.put( 'O', new Node( 1016f, 1017f, 100f ) );
+        nTable.put( 'P', new Node( 1018f, 1019f, 100f ) );
+        nTable.put( 'Q', new Node( 1020f, 1021f, 190f ) );
+        nTable.put( 'R', new Node( 1022f, 1023f, 100f ) );
+        nTable.put( 'S', new Node( 1024f, 1025f, 130f ) );
+        nTable.put( 'T', new Node( 1026f, 1027f, 100f ) );
+        nTable.put( 'U', new Node( 1028f, 1029f, 100f ) );
+        nTable.put( 'V', new Node( 1030f, 1031f, 100f ) );
+        nTable.put( 'W', new Node( 1032f, 1033f, 100f ) );
+        nTable.put( 'X', new Node( 1034f, 1035f, 120f ) );
+        nTable.put( 'Y', new Node( 1036f, 1037f, 140f ) );
+        nTable.put( 'Z', new Node( 1038f, 1039f, 100f ) );
+
+        for (Character ch = 'A'; ch <= 'Z'; ch++) {
+            mnfld.addPipe( nTable.get( ch ) );
+        }
+
+        // Inserting Edges to the graph
+        mnfld.insertConnection( new Edge( nTable.get( 'A' ), nTable.get( 'G' ), 300f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'A' ), nTable.get( 'H' ), 300f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'B' ), nTable.get( 'H' ), 300f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'B' ), nTable.get( 'K' ), 300f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'B' ), nTable.get( 'Z' ), 300f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'C' ), nTable.get( 'Z' ), 300f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'C' ), nTable.get( 'L' ), 300f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'G' ), nTable.get( 'I' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'G' ), nTable.get( 'J' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'G' ), nTable.get( 'H' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'H' ), nTable.get( 'K' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'K' ), nTable.get( 'Z' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'K' ), nTable.get( 'J' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'Z' ), nTable.get( 'L' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'L' ), nTable.get( 'M' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'I' ), nTable.get( 'N' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'J' ), nTable.get( 'M' ), 1f, true ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'J' ), nTable.get( 'Y' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'M' ), nTable.get( 'O' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'M' ), nTable.get( 'X' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'N' ), nTable.get( 'Y' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'N' ), nTable.get( 'Q' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'Y' ), nTable.get( 'O' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'Y' ), nTable.get( 'P' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'Q' ), nTable.get( 'T' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'Q' ), nTable.get( 'S' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'P' ), nTable.get( 'U' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'P' ), nTable.get( 'S' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'O' ), nTable.get( 'U' ), 1f, true ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'S' ), nTable.get( 'V' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'U' ), nTable.get( 'V' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'U' ), nTable.get( 'W' ), 1f, true ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'X' ), nTable.get( 'R' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'R' ), nTable.get( 'W' ), 1f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'U' ), nTable.get( 'E' ), 300f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'V' ), nTable.get( 'E' ), 300f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'T' ), nTable.get( 'D' ), 300f ) );
+        mnfld.insertConnection( new Edge( nTable.get( 'W' ), nTable.get( 'F' ), 300f ) );
+    }
+
+    public static void liveDemo() {
+        // 0->8
+        // Tanks 0, 2, 4
+        Scanner in = new Scanner( System.in );
+        float srcTank1, srcTank2, destTank;
+        int alternetPaths;
+        Graph mnfld = new Graph();
+        boolean runSearch = true;
+
+        //might need to limit query results, too large maybe?
+        //newnodes = JDBC.graphInformation(); //returns arraylist of nodes
+        //mnfld.setPipes(newnodes); //set nodes to the manifold
+        //JDBC.insertConnections(mnfld);
+
+//		 /*
+//			for (int i = 0; i < newnodes.length(); i++) { //length might be wrong
+//				//need to look for way to add edges by looking for neighbor nodes i think
+//				//loop through nodes and create Edges
+//				//might need new query
+//				newedges.addEdge(node1,node2,cost);
+//			}
+//
+//			mnfld.setConnections(newedges);
+//
+//			up to this point, graph should be global to Dijkstra and unused.
+//			loop until user leaves page
+//			get input from user for tanks to transfer
+//			find shortest path between both tanks
+//			end loop
+//			this will change depending how html works so not spending any time now on it
+//		*/
+        //shortestPath.runTest();
+
+        shortestPath.loadGraph( mnfld );
+        while (runSearch) {
+            Dijkstra.resetCosts( mnfld );
+
+            // Gets user input
+            System.out.print( "First Source Tank: " );
+            srcTank1 = in.nextFloat();
+            System.out.print( "Second Source Tank: " );
+            srcTank2 = in.nextFloat();
+            System.out.print( "Destination Tank: " );
+            destTank = in.nextFloat();
+            System.out.print( "Desired Number of Alternate Paths: " );
+            alternetPaths = in.nextInt();
+
+            // Prints outputs
+            System.out.println( "True shortest with alt paths" );
+            Dijkstra.findAltPaths( mnfld, 3, srcTank1, destTank );
+            mnfld.restoreDroppedConnections();
+            Dijkstra.resetCosts( mnfld );
+
+            System.out.println( "Lineup Considering used connections" );
+            Dijkstra.findMinPaths( mnfld, mnfld.getPipe( srcTank1 ), true );
+            mnfld.getPipe( destTank ).printLine();
+            mnfld.restoreDroppedConnections();
+            Dijkstra.resetCosts( mnfld );
+
+            System.out.println( "Lineup Considering only open connections" );
+            Dijkstra.findMinPaths( mnfld, mnfld.getPipe( srcTank1 ), false );
+            mnfld.getPipe( destTank ).printLine();
+            mnfld.restoreDroppedConnections();
+            Dijkstra.resetCosts( mnfld );
+
+            System.out.println( "Merge Lineup" );
+            Dijkstra.findMinPaths( mnfld, mnfld.getPipe( srcTank1 ) );
+            System.out.println( "\t Original Lineup: " );
+            mnfld.getPipe( destTank ).printLine();
+            System.out.println( "\t Merged Lineup: " );
+            Dijkstra.mergePaths( mnfld, srcTank2, mnfld.getPipe( destTank ).getPath(), mnfld.getPipe( destTank ) );
+            System.out.println( "\nRun another search [1:Yes, 0:No]" );
+            if (in.nextInt() == 0) runSearch = false;
+            else System.out.println( "\n\n\n" );
+        }
+
+
+    }
 
 }
